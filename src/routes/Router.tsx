34b077b2c6c59c69d421249746bee72ba2a976cf
2theMoon <dlaguddh1@gmail.com>
2023-02-24 22:07:58 +0900

    배포 수정

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,14 +1,12 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import CoinDetail from "./CoinDetail";
 import Home from "./Home";
-import NotFound from "./NotFound";
 
 function Router(){
 
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path="*" element={<NotFound />} />
         <Route path="/" element={<Home />}/>
         <Route path="/detail/:coinId/*" element={<CoinDetail />}/>
       </Routes>
